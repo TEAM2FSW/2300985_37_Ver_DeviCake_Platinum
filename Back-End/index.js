26019ad14fcbd84792425969c3fd59fd75ea272f
verrelprayogii2@gmail.com <verrelprayogii2@gmail.com>
2023-11-09 22:21:10 +0800

    2112

--- Back-End/index.js
+++ Back-End/index.js
@@ -1,23 +1,9 @@
 const express = require('express');
-const router = require('./routers/router');
-const path = require("path");
-const productRoutes = require('./routers/productRoutes');
-const morgan = require('morgan');
-
 const app = express();
 const port = 3000;
 
 
-app.use(morgan('dev'));
 
-app.use(express.json());
-app.use(express.urlencoded({
-    extended: false
-}));
-
-app.use(router);
-
-app.use('/products', productRoutes);
 
 app.listen(port, () => {
     console.log(`Server berjalan di port ${port}!`);
