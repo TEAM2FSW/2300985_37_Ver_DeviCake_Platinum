c2ba71321f8d9bc093e80b3028f47f8e1efb51cd
verrelprayogii2@gmail.com <verrelprayogii2@gmail.com>
2023-11-24 12:15:48 +0800

    Footer

--- Front-End/src/components/DataForm.jsx
+++ Front-End/src/components/DataForm.jsx
@@ -113,52 +113,7 @@ const Checkout = () => {
                   </div>
                 </div>
               </li>
-              <li className="flex py-6">
-                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                  <img
-                    src="https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    alt="Product"
-                    className="h-full w-full object-cover object-center rounded"
-                  />
-                </div>
-
-                <div className="ml-4 flex flex-1 flex-col">
-                  <div>
-                    <div className="flex justify-between text-base font-medium text-gray-900">
-                      <h3>Cheese Cake</h3>
-                      <p className="ml-4">Rp.200.000.000</p>
-                    </div>
-                    <p className="mt-1 text-sm text-gray-500">Cheese Cake</p>
-                  </div>
-                  <div className="flex flex-1 items-end justify-between text-sm">
-                    <p className="text-gray-500">Qty 1</p>
-                    <p className="text-pink-400 font-semibold inline-block">Rp.200.000.000</p>
-                  </div>
-                </div>
-              </li>
-              <li className="flex py-6">
-                <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                  <img
-                    src="https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                    alt="Product"
-                    className="h-full w-full object-cover object-center rounded"
-                  />
-                </div>
-
-                <div className="ml-4 flex flex-1 flex-col">
-                  <div>
-                    <div className="flex justify-between text-base font-medium text-gray-900">
-                      <h3>Cheese Cake</h3>
-                      <p className="ml-4">Rp.200.000.000</p>
-                    </div>
-                    <p className="mt-1 text-sm text-gray-500">Cheese Cake</p>
-                  </div>
-                  <div className="flex flex-1 items-end justify-between text-sm">
-                    <p className="text-gray-500">Qty 1</p>
-                    <p className="text-pink-400 font-semibold inline-block">Rp.200.000.000</p>
-                  </div>
-                </div>
-              </li>
+           
             </ul>
             <div className="px-8 border-b">
               {/* Total */}
