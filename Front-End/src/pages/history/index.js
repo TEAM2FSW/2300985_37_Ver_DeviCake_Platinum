4e6929c72ea21c0dfa89c8299912e8bfb75886d0
verrelprayogii2@gmail.com <verrelprayogii2@gmail.com>
2023-11-29 12:47:13 +0800

    Navbar,Order

--- Front-End/src/pages/history/index.js
+++ Front-End/src/pages/history/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import History from '@/components/History';
-import Navbar2 from '@/components/Navbar2';
+import Navbar from '@/components/Navbar';
 import { getCookie, setCookie } from "@/utils/cookies";
 import { useRouter } from 'next/router';
 
@@ -18,7 +18,7 @@ const HistoryPage = () => {
   }, [router]);
   return (
     <div>
-      <Navbar2/>
+      <Navbar/>
       <History/>
     </div>
   );
