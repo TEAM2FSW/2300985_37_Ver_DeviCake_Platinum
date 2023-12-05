fab29563ad061b28d2ed30f14cd843e44c3151c4
IqbalSaliAlparissy12 <iqbalsalialparissy@gmail.com>
2023-12-05 16:11:10 +0700

    frontend dipisahkan

--- Front-End/src/rest/api.js
+++ Front-End/src/rest/api.js
@@ -66,15 +66,15 @@ export const postRegistrtion = async ({
   }
 };
 
-// export const getCakes = async () => {
-//   try {
-//     await getToken();
-//     const response = await api.get("/api/cakes");
-//     return response.data.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
+export const getCakes = async () => {
+  try {
+    await getToken();
+    const response = await api.get("/api/cakes");
+    return response.data.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const postCartItem = async ({ cakeId, quantity }) => {
   try {
@@ -219,92 +219,3 @@ export const deleteAlamat = async (address_id) => {
   }
 };
 
-
-export const getCakes = async () => {
-  try {
-      const response = await api.get('/api/cakes');
-      return response.data.data;
-  } catch (error) {
-      throw error;
-  }
-}
-
-export const postCakes = async ({
-  name, description, price, image, category
-}) => {
-  try {
-      const response = await api.post('/api/cakes', {
-        user_id: 3,
-        name,
-        description,
-        price,
-        category,
-        image: image
-      });
-      return response.data;
-  } catch (error) {
-      if (error) {
-          alert(error.response.data.message);            
-      }
-  }
-}
-
-export const deleteCake = async ({ cake_id }) => {
-  try {
-      const response = await api.delete('/api/cakes/' + cake_id);
-      return response.data;
-  } catch (error) {
-      if (error) {
-          alert(error.response.data.message);            
-      }
-  }
-}
-
-
-export const getOrders = async () => {
-  try {
-      const response = await api.get('/api/orders');
-      return response.data.data;
-  } catch (error) {
-      throw error;
-  }
-}
-
-export const getCountOrders = async () => {
-  try {
-      const response = await api.get('/api/orders/count');
-      return response.data.data;
-  } catch (error) {
-      throw error;
-  }
-}
-
-export const getUsers = async () => {
-  try {
-      const response = await api.get('/api/users');
-      return response.data.data;
-  } catch (error) {
-      throw error;
-  }
-}
-
-export const getCountUsers = async () => {
-  try {
-      const response = await api.get('/api/users/count');
-      return response.data.data;
-  } catch (error) {
-      throw error;
-  }
-}
-
-
-export const deleteUser = async ({ user_id }) => {
-  try {
-      const response = await api.delete('/api/users/' + user_id);
-      return response.data;
-  } catch (error) {
-      if (error) {
-          alert(error.response.data.message);            
-      }
-  }
-}
