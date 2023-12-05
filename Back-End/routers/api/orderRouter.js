c3077922a243e97c6c3161404fc37922e2e42557
IqbalSaliAlparissy12 <iqbalsalialparissy@gmail.com>
2023-12-05 15:57:07 +0700

    last

--- Back-End/routers/api/orderRouter.js
+++ Back-End/routers/api/orderRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const orderRouter = express.Router();
 const OrderController = require('../../controllers/orderController');
 const orderController = new OrderController();
-const checkToken = require("../../middle/checkToken");
+const checkToken = require("../../middlewares/checkToken");
 
 /**
  * @swagger
