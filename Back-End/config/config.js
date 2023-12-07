9cb30ea86a67e8b943752bd7ce0070e2deafd536
Verrelp <verrelprayogii2@gmail.com>
2023-12-07 13:08:19 +0000

    push

--- Back-End/config/config.js
+++ Back-End/config/config.js
@@ -24,10 +24,18 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+  "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
+    "dialect": "postgres",
+    "timezone": "+07:00",
+    "dialectOptions": {
+      "ssl": {
+        "require": true, 
+        "rejectUnauthorized": false
+      },
+      "sslmode": "require"
+    }
+}
 }
