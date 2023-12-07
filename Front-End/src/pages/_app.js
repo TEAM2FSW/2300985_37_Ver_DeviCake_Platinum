7279c4dfc88e14c9c5fee198e70ac9d5fb303dad
ersancakra <ersancakra@gmail.com>
2023-12-07 21:32:24 +0700

    push update

--- Front-End/src/pages/_app.js
+++ Front-End/src/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import Footer from '@/components/Footer';
 import { useRouter } from 'next/router';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
